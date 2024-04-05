84cbf81f7adcf84eb7d45885672c17b76351bd1e
serkanboyraci <serkanboyraci@gmail.com>
2024-04-05 11:57:40 +0200

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Ali serkan Boyracı  on 5.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
